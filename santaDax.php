acdf20a21d77dd89ddc5bbda4c1e56adc81426be
Eric Lalitte <eric@lalitte.com>
2018-11-13 17:16:04 +0100

    Ajout de Dax et d'un index

--- santaDax.php
+++ santaDax.php
@@ -10,43 +10,16 @@ function envoiMail($adresseMail, $prochaineAdresse) {
 	    case "m.duarte@intech-sud.fr":
 		$cadeau = "Marie";
         	break;
-	    case "e.lalitte@intech-sud.fr":
+	    case "c.louberry@intech-sud.fr":
+		$cadeau = "Christine";
+        	break;
+	    case "m.flausino@intech-sud.fr":
 		$cadeau = "Marc";
         	break;
-	    case "e.lalitte@intech-sud.fr":
-		$cadeau = "Eric";
-        	break;
-//	    case "eric@lalitte.com":
-//		$cadeau = "Eric";
-//        	break;
-//	    case "juliette.blestel@gmail.com":
-//		$cadeau = "Juliette";
-//        	break;
-//	    case "dbenjamin@gmail.com":
-//		$cadeau = "François";
-//        	break;
-//	    case "Pierrot@lalitte.com":
-//		$cadeau = "Pierrot";
-//        	break;
-//	    case "annemarie@lalitte.com":
-//		$cadeau = "Anne-Marie";
-//        	break;
-//	    case "nathalie@lalitte.com":
-//		$cadeau = "Nath";
-//        	break;
-//	    case "maguy.lucas@laposte.net":
-//		$cadeau = "Maguy";
-//        	break;
-//	    case "mathieuchauveau@hotmail.com":
-//		$cadeau = "Mathieu";
-//        	break;
-//	    case "gwen.chauveau@gmail.com":
-//		$cadeau = "Gwen";
-//        	break;
 	}
 
         $to = $adresseMail;
-	$subject = "Votre cadeau de Noël des Lalitte/Lucas/Blestel !";
+	$subject = "Votre cadeau de Noël d'IN'TECH Dax !";
 	
 	$message = "
 	<html>
@@ -54,11 +27,11 @@ function envoiMail($adresseMail, $prochaineAdresse) {
 	    <title>A qui devrez-vous faire un cadeau ?</title>
 	  </head>
 	  <body>
-	    <p>Chère famille,</p>
+	    <p>Chèr.es amis,</p>
 	    <p>Si vous recevez ce mail, c'est que le père Noël vous a choisi.</p>
 	    <p>Comme convenu, à ce Noël, vous n'aurez à faire qu'un seul cadeau à une seule personne. Même si rien ne vous empêche d'en faire d'autres.</p>
-	    <p>Il faut donc que votre cadeau soit formidable !</p> 
-	    <p>Mais comme vous n'en avez qu'un seul à faire, vous aurez le temps pour vous creuser les méninges, investir, et faire plaisir à l'heureux élu.</p>
+	    <p>Il faut donc que votre cadeau soit formidable ! mais coûte moins de 10€.</p> 
+	    <p>Mais comme vous n'en avez qu'un seul à faire, vous aurez le temps pour vous creuser les méninges et faire plaisir à l'heureux élu.</p>
 	    <p>Et d'ailleurs, l'heureux élu à qui vous devrez faire le cadeau est : <strong>".$cadeau."</strong>.</p>
 	    <p>A vous de lui faire plaisir !</p>
 	    <img src='https://blackburnempire.com/TMNEW/wp-content/uploads/2016/12/Santa-Needs-You.jpg'>
@@ -69,7 +42,7 @@ function envoiMail($adresseMail, $prochaineAdresse) {
 	$headers[] = 'MIME-Version: 1.0';
 	$headers[] = 'Content-type: text/html; charset=UTF-8';
 	//$headers[] = 'To: elalitte@gmail.com';
-	$headers[] = 'From: Père Noël des Lalitte <eric@lalitte.com>';
+	$headers[] = 'From: Père Noël de Dax <eric@lalitte.com>';
 	
 	$success = mail($to, $subject, $message, implode("\r\n", $headers));
 	if (!$success) {
@@ -78,12 +51,12 @@ function envoiMail($adresseMail, $prochaineAdresse) {
 }
 
 // On liste les utilsateurs dans un tableau
-$listUsers=["eric@lalitte.com", "annemarie@lalitte.com", "gwen.chauveau@gmail.com", "maguy.lucas@laposte.net", "pierrot@lalitte.com", "nathalie@lalitte.com", "dbenjamain@gmail.com", "juliette.blestel@gmail.com", "mathieuchauveau@hotmail.com"];
+$listUsers=["e.lalitte@intech-sud.fr", "m.duarte@ntech-sud.fr", "m.flausino@intech-sud.fr", "c.louberry@intech-sud.fr"];
 //$listUsers=["eric@lalitte.com", "elalitte@fastmail.fm", "elalitte@gmail.com"];
 // On les mélange
 shuffle($listUsers);
 // On ouvre un fichier pour stocker l'ordre de la chaine
-$monfichier = fopen('chaineNoel.txt', 'r+');
+$monfichier = fopen('chaineNoelDax.txt', 'r+');
 
 // On parcourt la chaine adresse par adresse
 for ($i = 0; $i < count($listUsers); $i++) {
